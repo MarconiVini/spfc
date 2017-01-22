8ab99a766be8e36252ff7b197b487608240113ac
Marcos Vinicius Cardoso Santini <cardosounicamp@gmail.com>
2017-01-22 21:24:10 -0200

    post with owner

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -2,11 +2,13 @@ FactoryGirl.define do
   factory :post do
     body { Faker::Lorem.paragraphs }
     
-    #after(:create) do |post|
-    #  user = FactoryGirl.build(:user)
-    #  user.posts << post
-    #  user.save
-    #  post.owner ||= user
-    #end
+    factory :post_with_owner do
+     after(:build) do |post|
+       user = FactoryGirl.build(:user)
+       user.posts << post
+       user.save
+       post.owner ||= user
+      end 
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Post, :type => :model do 
   let(:post_build) { build(:post) }
+  let(:post_with_owner) { build(:post_with_owner) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
   
   it 'have a body text' do
     text = "hooo new post"
-    post_build.body = text
-    post_build.save!
-    expect(post_build.body).to eq text
+    post_with_owner.body = text
+    post_with_owner.save!
+    expect(post_with_owner.body).to eq text
   end
 
   it 'belongs to a user' do
